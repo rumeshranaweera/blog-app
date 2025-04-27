4293079a1d7e52a2d1cd5cc9e59246a0cfe6f303
rumesh <rumeshranaweera99@gmail.com>
2025-04-27 23:56:09 +0530

    update : next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com", "media.istockphoto.com"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
